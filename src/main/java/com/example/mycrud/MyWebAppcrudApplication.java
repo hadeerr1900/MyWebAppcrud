398f904932361f9595d8f9abf5a0056ff90b60f2
hadeer khalaf <60439912+hadeerr1900@users.noreply.github.com>
2023-01-12 00:48:39 +0200

    frst app

---
+++ src/main/java/com/example/mycrud/MyWebAppcrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.mycrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebAppcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebAppcrudApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/mycrud/service/EmployeeService.java
@@ -0,0 +1,46 @@
+package com.example.mycrud.service;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.mycrud.entity.Employee;
+import com.example.mycrud.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+	
+	
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	
+	 public List < Employee > getAllEmployees() {
+	        return employeeRepository.findAll();
+	    }
+
+	  
+	    public void saveEmployee(Employee employee) {
+	        this.employeeRepository.save(employee);
+	    }
+
+	   
+	    public Employee getEmployeeById(long id) {
+	        Optional < Employee > optional = employeeRepository.findById(id);
+	        Employee employee = null;
+	        if (optional.isPresent()) {
+	            employee = optional.get();
+	        } else {
+	            throw new RuntimeException(" Employee not found for id :: " + id);
+	        }
+	        return employee;
+	    }
+
+	    
+	    public void deleteEmployeeById(long id) {
+	        this.employeeRepository.deleteById(id);
+	    }
+
+}
